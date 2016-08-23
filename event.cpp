a431cce7e5c0f6e8c103f133a3880bdd9e8c9bd3
lucinda-aemilia <lucinda.aemilia@gmail.com>
2016-08-23 16:15:19 +0800

    Finished up with writing events

--- event.cpp
+++ event.cpp
@@ -43,3 +43,42 @@ void Event::setEndDate(const QDateTime &endDate)
         emit endDateChanged(mEndDate);
     }
 }
+
+QString Event::description() const
+{
+    return mDescription;
+}
+
+void Event::setDescription(const QString &description)
+{
+    if (description != mDescription) {
+        mDescription = description;
+        emit descriptionChanged(mDescription);
+    }
+}
+
+QString Event::location() const
+{
+    return mLocation;
+}
+
+void Event::setlocation(const QString &location)
+{
+    if (location != mLocation) {
+        mLocation = location;
+        emit locationChanged(mLocation);
+    }
+}
+
+QColor Event::color() const
+{
+    return mColor;
+}
+
+void Event::setcolor(const QString &color)
+{
+    if (color != mColor) {
+        mColor = color;
+        emit colorChanged(mColor);
+    }
+}
