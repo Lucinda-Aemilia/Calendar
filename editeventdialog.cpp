bba45bc1639fa06cf50cc6a6e0802b8735752d0c
lucinda-aemilia <lucinda.aemilia@gmail.com>
2016-08-25 10:46:59 +0800

    写ui……

--- editeventdialog.cpp
+++ editeventdialog.cpp
@@ -22,16 +22,22 @@ EditEventDialog::~EditEventDialog()
     delete ui;
 }
 
-void EditEventDialog::init(Event *event)
+void EditEventDialog::init(Event *event, const QDateTime& startDate,
+                           const QDateTime& endDate)
 {
     if (event != NULL)
+    {
         ui->dialogButtonBox->addButton(QDialogButtonBox::Save);
+    }
+
+    // 设置初值（如果是edit）或者默认值
 }
 
 // 结束的时间日期必须比开始的时间日期大
 void EditEventDialog::on_startDateTimeEdit_dateTimeChanged(const QDateTime &dateTime)
 {
     ui->endDateTimeEdit->setMinimumDateTime(dateTime);
+    mStartDate = dateTime;
 }
 
 // 是否显示关于重复的groupbox
@@ -53,6 +59,7 @@ void EditEventDialog::on_endDateTimeEdit_dateChanged(const QDate &date)
 }
 
 // 如果为全天，则使两个datetimeedit disable。
+// 设置开始和结束时间
 void EditEventDialog::on_allDayCheckBox_stateChanged(int arg1)
 {
     if (arg1 == Qt::Unchecked)
@@ -64,5 +71,77 @@ void EditEventDialog::on_allDayCheckBox_stateChanged(int arg1)
     {
         ui->startDateTimeEdit->setDisabled(true);
         ui->endDateTimeEdit->setDisabled(true);
+        ui->startDateTimeEdit->setDateTime(QDateTime(ui->startDateTimeEdit->date(), QTime(0, 0)));
+        ui->endDateTimeEdit->setDateTime(QDateTime(ui->startDateTimeEdit->date(), QTime(23, 59)));
     }
+}
+
+// 改变后面显示的星期数
+void EditEventDialog::on_repeatEndDateEdit_dateChanged(const QDate &date)
+{
+    ui->repeatEndDateLabel->setText(getWeekDayName(date));
+}
+
+void EditEventDialog::on_eventNameLineEdit_textChanged(const QString &arg1)
+{
+    mName = arg1;
+}
+
+// 改结束时间
+void EditEventDialog::on_endDateTimeEdit_dateTimeChanged(const QDateTime &dateTime)
+{
+    mEndDate = dateTime;
+}
+
+void EditEventDialog::on_locationLineEdit_textChanged(const QString &arg1)
+{
+    mLocation = arg1;
+}
+
+void EditEventDialog::on_descriptionTextEdit_textChanged()
+{
+    mDescription = ui->descriptionTextEdit->toPlainText();
+}
+
+void EditEventDialog::on_clorComboBox_currentIndexChanged(const QString &arg1)
+{
+    mColor = QColor(arg1);
+}
+
+
+
+void EditEventDialog::on_startDateTimeEdit_dateChanged(const QDate &date)
+{
+    ui->repeatTimeLabel->setText(getWeekDayName(date));
+}
+
+QString EditEventDialog::getWeekDayName(const QDate &date)
+{
+    QString dayName;
+    switch (date.dayOfWeek())
+    {
+    case 1:
+        dayName = QObject::tr("Monday");
+        break;
+    case 2:
+        dayName = QObject::tr("Tuesday");
+        break;
+    case 3:
+        dayName = QObject::tr("Wednesday");
+        break;
+    case 4:
+        dayName = QObject::tr("Thursday");
+        break;
+    case 5:
+        dayName = QObject::tr("Friday");
+        break;
+    case 6:
+        dayName = QObject::tr("Saturday");
+        break;
+    case 7:
+        dayName = QObject::tr("Sunday");
+        break;
+    }
+
+    return dayName;
 }
--- editeventdialog.h
+++ editeventdialog.h
@@ -17,7 +17,8 @@ public:
     enum EditFlag { create, edit };
     explicit EditEventDialog(QWidget *parent = 0);
     ~EditEventDialog();
-    void init(Event* event = NULL);
+    void init(Event* event = NULL, const QDateTime &startDate = QDateTime::currentDateTime(),
+              const QDateTime &endDate = QDateTime::currentDateTime().addSecs(3600));
 
 private slots:
     void on_startDateTimeEdit_dateTimeChanged(const QDateTime &dateTime);
@@ -28,7 +29,23 @@ private slots:
 
     void on_allDayCheckBox_stateChanged(int arg1);
 
+    void on_repeatEndDateEdit_dateChanged(const QDate &date);
+
+    void on_eventNameLineEdit_textChanged(const QString &arg1);
+
+    void on_endDateTimeEdit_dateTimeChanged(const QDateTime &dateTime);
+
+    void on_locationLineEdit_textChanged(const QString &arg1);
+
+    void on_descriptionTextEdit_textChanged();
+
+    void on_clorComboBox_currentIndexChanged(const QString &arg1);
+
+    void on_startDateTimeEdit_dateChanged(const QDate &date);
+
 private:
+    QString getWeekDayName(const QDate& date);
+
     Ui::EditEventDialog *ui;
     EditFlag mEditFlag;
     QString mName;
